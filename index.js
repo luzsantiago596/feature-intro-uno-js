fc6b41489edd6ba5cf545a77267926a26a333a2b
Luz <luz7282@gmail.com>
2022-11-08 20:23:06 -0300

    Ejercicio en JS

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombre_ciudad = 'junin';
+let num = 21;
+nombre_ciudad = num;
+console.log(nombre_ciudad, num);
+
+const nombre = "luz";
+const apellido = "santiago";
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
